dd1c3ade4f6cc77c19e6b4cf9b3e81b830cf8b73
surendhirans <surendhiransam@gmail.com>
2022-03-03 11:48:54 +0530

    project java full stack

---
+++ project-name/src/index.js
@@ -0,0 +1,15 @@
+import React from'react'
+import ReactDom from'react-dom'
+
+//render("what to show, where to show")
+ReactDom.render(
+    <>
+<h1>hi </h1>
+<p>Surendhiran</p>
+<h2>nala irkiya machan</h2>
+<p>Soldran machan</p>
+<h3>vankam da mapala</h3>
+<p>ne soldrathu ellam poi than</p>
+</>
+,document.getElementById("root")
+)
